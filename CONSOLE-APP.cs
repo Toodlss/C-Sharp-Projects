9f63948e4921204496dcdc8ad1e9fd0472f24d1c
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-09-15 11:48:28 -0700

    Update CONSOLE-APP.cs

--- CONSOLE-APP.cs
+++ CONSOLE-APP.cs
@@ -1,6 +1,7 @@
+// sets the initial value of number to 500 and number2 to 85
 int number = 500;
 int number2 = 85;
-
+// uses a do-while loop to subtract number2 from number until number is less than or equal to 0
 do
 {
     number -= number2;
